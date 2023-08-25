37fb26afc09187f0f9424a0a344637c921c077b1
Vu <longvunguyen2003@gmail.com>
2023-08-25 11:42:19 +0700

    EXERCISE 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (string.length == 0){
+        return ``;
+    }
+    let result = ``;
+    for (let i = string.length - 1; i > -1; i--){
+        result += string[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
