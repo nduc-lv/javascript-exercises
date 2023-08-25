418f8f8f35bfe1a80bb2e6abf4f44700d265b2e0
Vu <longvunguyen2003@gmail.com>
2023-08-25 11:38:29 +0700

    EXERCISE 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0){
+        return `ERROR`;
+    }
+    if (string ==  ``){
+        return ``;
+    }
+    let result = ``;
+    for (; number > 0; number--){
+        result += `hey`;
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
