a5a1492cb42b47b074e581ddcd70ca47afd1aa62
Vu <longvunguyen2003@gmail.com>
2023-08-25 15:37:45 +0700

    EXERCISE 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+//verify input
+//fibonacci formula
+const fibonacci = function(position) {
+    //verify input
+    if (position <= 0){
+        return "OOPS";
+    }
+    //fibonacci formula
+    let first = 1;
+    let second = 1;
+    let current = 3;
+    let result = 1;
+    while (current <= position){
+        result = first + second;
+        first = second;
+        second = result;
+        current++;
+    }
+    return result;
 };
 
 // Do not edit below this line
