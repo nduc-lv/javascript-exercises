681ed6ff3b5c381b618958ea8ccb155ad8f803f9
Vu <longvunguyen2003@gmail.com>
2023-08-25 15:02:13 +0700

    EXERCISE 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,28 @@
-const convertToCelsius = function() {
+//verify the input
+//use the formulas to convert
+//round the result to one decimal place
+const convertToCelsius = function(number) {
+  //verify the input
+  if (typeof number != `number`){
+    return `ERROR`;
+  }
+  //use the formula to convert
+  let result = (number - 32) * 5 / 9;
+  
+  //return the rounded result
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  //verify the input
+  if (typeof number != `number`){
+    return `ERROR`;
+  }
+  //use the formula to convert
+  let result = number * 9 / 5 + 32;
+  
+  //return the rounded result
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
