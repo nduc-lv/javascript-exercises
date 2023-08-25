3ed9a93b875024819c6dcff7b3016de5d5926869
Vu <longvunguyen2003@gmail.com>
2023-08-25 14:46:48 +0700

    EXERCISE 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+//make sure the year is valid -> return ERROR if it's not
+//return true if the given year satisfies leap year's conditions, else false
+const leapYears = function(year) {
+    //verify the year
+    if (typeof year != `number` || year <= 0){
+        return "ERROR";
+    }
+    //check the conditions
+    if (year % 400 == 0){
+        return true;
+    }
+    if ((year % 100 != 0) && (year % 4 == 0)){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
