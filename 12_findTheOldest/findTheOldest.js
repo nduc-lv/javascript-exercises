710f1b9a07c03eb931b8ed02886cef48cc3c514c
Vu <longvunguyen2003@gmail.com>
2023-08-25 16:08:22 +0700

    EXERCISE 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
-
+//need to find the most recent year to calculate age for people who still live
+//calculate age and return the oldest person
+const findTheOldest = function(array) {
+    //find the most recent year
+    let theMostRecentYear = 0;
+    array.forEach(element => {
+        let currentYear = element.yearOfBirth;
+        if (element.yearOfDeath != undefined){
+            currentYear = element.yearOfDeath;
+        }
+        if (theMostRecentYear < currentYear){
+            theMostRecentYear = currentYear;
+        }
+    });
+    //calculate age
+    let result = array[0];
+    let max = 0;
+    array.forEach(element => {
+        let age = theMostRecentYear - element.yearOfBirth;
+        if (element.yearOfDeath != undefined){
+            age = element.yearOfDeath - element.yearOfBirth;
+        }
+        if (age > max){
+            max = age;
+            result = element;
+        }
+    });
+    return result;
 };
 
 // Do not edit below this line
