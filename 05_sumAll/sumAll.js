ece73125e499f68763b1f7e9332c0a65200bc3d4
Vu <longvunguyen2003@gmail.com>
2023-08-25 12:12:50 +0700

    EXERCISE 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    //make sure arguments are number
+    if ((typeof startNum) != "number" || (typeof endNum != "number")){
+        return "ERROR";
+    }
+    //make sure startNum and endNum > 0
+    if (startNum < 0  || endNum < 0){
+        return "ERROR";
+    }
+    //make sure startNum <= endNum
+    if (startNum > endNum){
+        let temp = startNum;
+        startNum = endNum;
+        endNum = temp;
+    }
 
+    //apply formula
+    let numberOfNum = (endNum - startNum) + 1;
+    return ((startNum + endNum) * numberOfNum) / 2; 
 };
 
 // Do not edit below this line
