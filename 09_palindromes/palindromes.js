fb36df3b5e94c04424b0e49d2ec80c2d4ea2707a
Vu <longvunguyen2003@gmail.com>
2023-08-25 15:30:00 +0700

    EXERCISE 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+//need to remove all non-alphabet character
+//check the symetric of the given string
+const palindromes = function (string) {
+    //remove none alphabet character
+    string = string.toLowerCase();
+    let result = ``;
+    for (const char of string){
+        if ((`a` <= char && char <= `z`) || (`0` <= char && char <= `9`)){
+            result += char;
+        }
+    }
+    //check the symetric
+    let length = result.length;
+    for (let i = 0; i < length / 2; i++){
+        if (result[i] != result[length - 1 - i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
