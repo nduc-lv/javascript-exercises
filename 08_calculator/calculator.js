9a8ea9a06961eb5816cf56a83a47344c0063dd3e
Vu <longvunguyen2003@gmail.com>
2023-08-25 15:15:11 +0700

    EXERCISE 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(...args) {
+	let result = 0;
+  for (let i = 0; i < args.length; i++){
+    result += +args[i];
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return +number1 - +number2;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	let result = 0;
+  for (let i = 0; i < args.length; i++){
+    result += +args[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let result = 1;
+  for (let i = 0; i < args.length; i++){
+    result *= args[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	return number1 ** number2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number1) {
+	let result = 1;
+  for (let i = 1; i <= number1; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
